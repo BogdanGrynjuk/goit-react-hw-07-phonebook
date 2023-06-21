1fca0efac9a544c60e552b7adc1620f111e225ba
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-06-22 00:44:45 +0300

    Adds Async Redux logic

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,37 +1,63 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { addContact, fetchContacts, deleteContact } from './operations';
 
-const contactsInitialState = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const handlePending = function(state) {
+  return {
+    ...state,
+    isLoading: true,
+  };  
+};
+
+const handleRejected = function (state, action) {
+  return {
+    ...state,
+    isLoading: false,
+    error: action.payload,
+  };
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: contactsInitialState,
-  reducers: {
-    
-    addContact: {
-      reducer(state, action) {        
-        return [action.payload, ...state]
-      },
-      prepare({name, number}) {
-        return {
-          payload: {
-            id: nanoid(),
-            name,
-            number,
-          }
-        }
-      }
-    },
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
 
-    deleteContact(state, action) {      
-      return state.filter(contact => contact.id !== action.payload)
-    },    
+  extraReducers: {
+    [fetchContacts.pending]: handlePending,
+    [fetchContacts.fulfilled](state, action) {      
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        items: action.payload,
+      };
+    },
+    [fetchContacts.rejected]: handleRejected,
+
+    [addContact.pending]: handlePending,
+    [addContact.fulfilled](state, action) {      
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        items: [action.payload, ...state.items],
+      };
+    },
+    [addContact.rejected]: handleRejected,
+
+    [deleteContact.pending]: handlePending,
+    [deleteContact.fulfilled](state, action) {
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        items: state.items.filter(item => item.id !== action.payload.id),
+      };      
+    },
+    [deleteContact.rejected]: handleRejected,
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,10 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  }
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
-
-export const persistor = persistStore(store);
